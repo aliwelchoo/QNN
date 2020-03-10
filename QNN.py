f5d3d789c85d39846055c178cb6e2628086fde94
Alistair Welch <aliwelchoo@live.com>
2020-03-10 15:43:59 +0000

    changed graph colour, added validation results

--- QNN.py
+++ QNN.py
@@ -53,12 +53,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 y_train = np.matrix(y_train)
 X_train = np.matrix(X_train)
-X_train = X_train/X_train.max(axis = 0)
+X_scale = X_train.max(axis = 0)
+X_train = X_train/X_scale
 
 TC = []
 NN = NeuralNetwork(X_train[0], y_train[0], 5)
 fig = plt.figure(num="LIVE", figsize=(14, 6), dpi=80, facecolor='w', edgecolor='k')
-for _ in range(500):
+for _ in range(100):
     C = []
     Result = []
     for it in range(len(X_train)) :
@@ -66,10 +67,21 @@ for _ in range(500):
         Result.append(NN.output - NN.y)
     TC.append(np.mean(C))
     plt.clf()
-    plt.plot(TC)
+    plt.plot(TC, color = 'black')
     fig.canvas.draw()
     fig.canvas.flush_events()
     
 plt.close()
 fig = plt.figure(num="Complete", figsize=(14, 6), dpi=80, facecolor='w', edgecolor='k')
-plt.plot(TC)
+plt.plot(TC, color = 'black')
+
+print("Training:" + str(int(len(X_train) - np.rint(Result).sum(axis = 0).sum())) + "/" + str(len(X_train)))
+
+y_test = np.matrix(y_test)
+X_test = np.matrix(X_test)
+X_test = X_test/X_scale
+Correct = []
+for it in range(len(X_test)) :
+    NN.step_train(X_test[it], y_test[it])
+    Correct.append((np.rint(NN.output) - NN.y).sum() == 0)
+print("Testing:" + str(np.array(Correct).sum()) + "/" + str(len(X_test)))
