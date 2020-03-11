efe9ff4c5788fea1835cb24ae27d2d9ee1ae6543
Alistair Welch <aliwelchoo@live.com>
2020-03-11 18:45:22 +0000

    created first QNN

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import numpy as np
-import Classic_NN as nn    
+import QNN
+#import Classic_NN as nn
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
@@ -17,31 +18,35 @@ X_scale = X_train.max(axis = 0)
 X_train = X_train/X_scale
 
 TC = []
-NN = nn.NeuralNetwork(X_train[0], y_train[0], 5)
+QNN = QNN.QuantumNeuralNetwork(X_train[0], y_train[0])
+plt.close()
 fig = plt.figure(num="LIVE", figsize=(14, 6), dpi=80, facecolor='w', edgecolor='k')
-for _ in range(100):
+for _ in range(500):
     C = []
     Result = []
     for it in range(len(X_train)) :
-        C.append(NN.step_train(X_train[it], y_train[it]))
-        Result.append(NN.output - NN.y)
+        C.append(QNN.step_train(X_train[it], y_train[it]))
+        Result.append(QNN.output - QNN.y)
     TC.append(np.mean(C))
     plt.clf()
+    plt.subplot(111) 
     plt.plot(TC, color = 'black')
+    plt.ylim(0,0.3)
     fig.canvas.draw()
     fig.canvas.flush_events()
     
 plt.close()
 fig = plt.figure(num="Complete", figsize=(14, 6), dpi=80, facecolor='w', edgecolor='k')
 plt.plot(TC, color = 'black')
+plt.ylim(0,0.3)
 
-print("Training:" + str(int(len(X_train) - np.rint(Result).sum(axis = 0).sum())) + "/" + str(len(X_train)))
+print("Training:" + str((np.matrix(np.rint(Result)).sum(axis = 1) == 0).sum()) + "/" + str(len(X_train)))
 
 y_test = np.matrix(y_test)
 X_test = np.matrix(X_test)
 X_test = X_test/X_scale
 Correct = []
 for it in range(len(X_test)) :
-    NN.step_train(X_test[it], y_test[it])
-    Correct.append((np.rint(NN.output) - NN.y).sum() == 0)
+    QNN.step_train(X_test[it], y_test[it])
+    Correct.append((np.rint(QNN.output) - QNN.y).sum() == 0)
 print("Testing:" + str(np.array(Correct).sum()) + "/" + str(len(X_test)))
--- QNN.py
+++ QNN.py
@@ -9,32 +9,38 @@ def sigmoid_derivative(x):
 class QuantumNeuralNetwork:
     def __init__(self, x, y):
         self.set_instance(x, y)
-        self.probs   = 0.5 * np.ones(self.input)
-        self.weights   = np.eye(2 * self.input,y.shape[1]) 
+        self.probs   = np.random.rand(self.input.shape[0])
+        self.weights   = np.eye(2 * self.input.shape[0],y.shape[1]) 
         self.output     = np.zeros(self.y.shape)
 
-#    def feedforward(self):
-#        self.layer1[:,1:] = sigmoid(np.dot(self.input.T, self.weights1))
-#        self.output = sigmoid(np.dot(self.layer1, self.weights2))
-#        return(sum(np.square(self.output - self.y)))
-#        
-#    def backprop(self, lr):
-#        # application of the chain rule to find derivative of the loss function with respect to probs and weights
-#        d_weights2 = np.dot(self.layer1.T, np.multiply(2*(self.y - self.output), (sigmoid_derivative(self.output))))
-#        d_weights1 = np.dot(self.input,  np.multiply(np.dot(np.multiply(2*(self.y - self.output), (sigmoid_derivative(self.output))), self.weights2[1:,:].T), sigmoid_derivative(self.layer1[:,1:])))
-#
-#        # update the weights with the derivative (slope) of the loss function
-#        self.weights1 += lr * d_weights1
-#        self.weights2 += lr * d_weights2
-#        
-#    def set_instance(self, x, y):
-#        inputx = np.ones((x.shape[1] + 1,1))
-#        inputx[1:] = x.T
-#        self.input      = inputx
-#        self.y          = y
-#        
-#    def step_train(self, x, y):
-#        self.set_instance(x, y)
-#        cost = self.feedforward()
-#        self.backprop(1)
-#        return cost
+    def feedforward(self):
+        self.layer1 = np.zeros(2 * self.input.shape[0])
+        for i in range(2 * self.input.shape[0]) :
+            self.layer1[i] = sigmoid(self.input[int(np.floor(i/2))]) * (i%2 + (2 * (i%2) - 1) * self.probs[int(np.floor(i/2))])
+        self.output = sigmoid(np.dot(self.layer1, self.weights))
+        self.layer1 = np.matrix(self.layer1)
+        return(sum(np.square(self.output - self.y)))
+        
+    def backprop(self, lr):
+        # application of the chain rule to find derivative of the loss function with respect to probs and weights
+        d_layer = np.matrix(np.multiply(2*(self.y - self.output), (sigmoid_derivative(self.output))))
+        d_weights = np.dot(self.layer1.T, d_layer)
+        d_probs = np.zeros(self.probs.shape)
+        for i in range(2 * self.input.shape[0]) :
+            d_probs[int(np.floor(i/2))] += (2 * (i%2) - 1) * sigmoid(self.input[int(np.floor(i/2))]) * np.multiply(np.multiply(d_layer, self.weights[i,:].T), self.layer1[:,i]).sum()
+
+        # update the weights with the derivative (slope) of the loss function
+        self.weights += lr * d_weights
+        self.probs += lr * d_probs
+        
+    def set_instance(self, x, y):
+        inputx = np.ones((x.shape[1] + 1,1))
+        inputx = x.T
+        self.input      = inputx
+        self.y          = y
+        
+    def step_train(self, x, y):
+        self.set_instance(x, y)
+        cost = self.feedforward()
+        self.backprop(0.3)
+        return cost
